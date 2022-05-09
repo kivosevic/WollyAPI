5d6346dbece71568a9cea45349b96bed1ef5c4a1
Katarina Ivosevic <k.ivosevic@vegait.rs>
2022-05-09 11:04:55 +0200

    added id to the cryptolist

---
+++ src/main/java/com/backend/backend/api/CryptocurrencyController.java
@@ -0,0 +1,14 @@
+package com.backend.backend.api;
+
+import io.swagger.annotations.Api;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/users")
+@RequiredArgsConstructor
+@Api(tags = "Cryptocurrencies")
+public class CryptocurrencyController {
+
+}
--- src/main/java/com/backend/backend/dto/GetCryptoListResponseDTO.java
+++ src/main/java/com/backend/backend/dto/GetCryptoListResponseDTO.java
@@ -4,10 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
+import java.util.UUID;
+
 @Data
 @AllArgsConstructor
 @Builder
 public class GetCryptoListResponseDTO {
+    private UUID id;
     private String name;
     private String icon;
     private Double valueOfOne;

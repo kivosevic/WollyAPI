220d8f919a8332a1fbfa04f1183caf8376c8f8da
Marko Stojakovic <marko.stojakovic@vegait.rs>
2022-06-24 11:42:51 +0200

    deleted unused imports

--- src/main/java/rs/vegait/wolly/WollyApiApplication.java
+++ src/main/java/rs/vegait/wolly/WollyApiApplication.java
@@ -2,9 +2,6 @@ package rs.vegait.wolly;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-// import org.springframework.boot.autoconfigure.domain.EntityScan;
-// import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-// import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication

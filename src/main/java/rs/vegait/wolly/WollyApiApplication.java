a3133115fce18e42f143b11379ddf63ceea8f618
Marko Stojakovic <marko.stojakovic@vegait.rs>
2022-06-27 10:34:31 +0200

    delete comments

--- src/main/java/rs/vegait/wolly/WollyApiApplication.java
+++ src/main/java/rs/vegait/wolly/WollyApiApplication.java
@@ -2,9 +2,11 @@ package rs.vegait.wolly;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
+@EnableAsync
 public class WollyApiApplication implements WebMvcConfigurer {
 
     public static void main(String[] args) {

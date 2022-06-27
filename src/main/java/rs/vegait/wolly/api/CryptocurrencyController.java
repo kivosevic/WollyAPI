bc370807871fc6f1b3822398a334bc71ff5b5fd0
Marko Stojakovic <marko.stojakovic@vegait.rs>
2022-06-27 10:41:06 +0200

    create service rapid api

--- src/main/java/rs/vegait/wolly/api/CryptocurrencyController.java
+++ src/main/java/rs/vegait/wolly/api/CryptocurrencyController.java
@@ -1,11 +1,6 @@
 package rs.vegait.wolly.api;
 
 import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
@@ -14,14 +9,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 import io.swagger.annotations.Api;
 import lombok.RequiredArgsConstructor;
-import rs.vegait.wolly.dto.CryptoDto;
 import rs.vegait.wolly.dto.GetCryptoListResponseDTO;
 import rs.vegait.wolly.service.CryptoService;
+import rs.vegait.wolly.service.RapidApiService;
 
 @RestController
 @RequestMapping("/api/v1/cryptos")
@@ -30,36 +22,21 @@ import rs.vegait.wolly.service.CryptoService;
 public class CryptocurrencyController {
 
     private final CryptoService cryptoService;
+    private final RapidApiService rapidApiService;
 
-    @GetMapping("/live")
-    public ResponseEntity<?> getLiveCrypto() throws IOException, InterruptedException {
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(
-                        "https://coingecko.p.rapidapi.com/coins/markets?vs_currency=usd&page=1&per_page=100&order=market_cap_desc"))
-                .header("X-RapidAPI-Key", "")
-                .header("X-RapidAPI-Host", "coingecko.p.rapidapi.com")
-                .method("GET", HttpRequest.BodyPublishers.noBody())
-                .build();
-        HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
-        List<CryptoDto> result = new ObjectMapper().readValue(response.body(),
-                new TypeReference<ArrayList<CryptoDto>>() {
-                });
-
-        cryptoService.addAllCryptoInDatabase(result);
-
-        return ResponseEntity.ok("Data is in database");
-    }
+    
 
     @GetMapping
     public ResponseEntity<?> getAllCrypto() throws IOException, InterruptedException {
 
         List<GetCryptoListResponseDTO> list = cryptoService.getAllCrypto();
+        rapidApiService.loadDataFromApi();
         return ResponseEntity.ok(list);
+        
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<?> getCrypto(@PathVariable("id") String id) {
         return ResponseEntity.ok(cryptoService.getCrypto(id));
     }
-
 }

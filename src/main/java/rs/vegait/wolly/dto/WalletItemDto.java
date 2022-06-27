6421990f6bc1a840681265b93cb754c640cfb3ad
Marko Stojakovic <marko.stojakovic@vegait.rs>
2022-06-27 10:56:56 +0200

    add wallet item dto

---
+++ src/main/java/rs/vegait/wolly/dto/WalletItemDto.java
@@ -0,0 +1,18 @@
+package rs.vegait.wolly.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+public class WalletItemDto {
+    
+    private String cryptocurrencyId;
+    private Double sum;
+}
